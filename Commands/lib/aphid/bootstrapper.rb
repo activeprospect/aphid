d54960e68b4af0f981114f9ca92909a56005f234
Justin Mecham <justin@mecham.net>
2010-10-05 18:19:16 -0400

    [Tools] Explicitly create the folders in the project skeleton when creating a new project with the "aphid" command, as we cannot guarantee that empty folders will be present in the Skeleton folder.

--- Commands/lib/aphid/bootstrapper.rb
+++ Commands/lib/aphid/bootstrapper.rb
@@ -4,6 +4,21 @@ module Aphid
   class Bootstrapper
 
     APHID_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..'))
+    SKELETON_STRUCTURE = [
+      "Application",
+      "Application/Controllers",
+      "Application/Models",
+      "Application/Views",
+      "Build",
+      "Library",
+      "Resources",
+      "Resources/Images",
+      "Resources/Stylesheets",
+      "Resources/Templates",
+      "Tests",
+      "Vendor",
+      "Vendor/Aphid"
+    ]
 
     attr_accessor :project_name
     attr_accessor :project_path
@@ -20,6 +35,9 @@ module Aphid
 
     def create_project_structure
       puts "Creating Project Structure ..."
+      SKELETON_STRUCTURE.each do |path|
+        Dir.mkdir "#{@project_path}/#{path}"
+      end
       Dir.clone! "#{APHID_ROOT}/Skeleton", @project_path, ["^\\."]
     end
 
@@ -51,7 +69,7 @@ class Dir
 
   def self.clone!(source, destination, exclude = [])
     exclude = ["^\\.{1,2}$"].concat(exclude).uniq
-    FileUtils.mkdir destination unless Dir.exists? destination
+    FileUtils.mkdir(destination) unless Dir.exists?(destination)
     Dir.foreach(source) do |file|
       next if exclude.detect { |pattern| file.match(/#{pattern}/i) }
 
@@ -59,7 +77,7 @@ class Dir
       destination_path = File.join(destination, file)
 
       if File.directory?(source_path)
-        FileUtils.mkdir(destination_path)
+        FileUtils.mkdir(destination_path) unless Dir.exists?(destination_path)
         clone! source_path, destination_path, exclude
       else
         FileUtils.cp(source_path, destination_path)
