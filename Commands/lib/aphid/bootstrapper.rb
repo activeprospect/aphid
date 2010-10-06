50a84e34c9544e66ba1e5e7d3fc12ce60b2cf3c3
Justin Mecham <justin@mecham.net>
2010-10-06 10:53:17 -0400

    [Tools] Use File.exists? instead of Dir.exists? in Aphid::Bootstrapper to support Ruby 1.8.7.

--- Commands/lib/aphid/bootstrapper.rb
+++ Commands/lib/aphid/bootstrapper.rb
@@ -69,7 +69,7 @@ class Dir
 
   def self.clone!(source, destination, exclude = [])
     exclude = ["^\\.{1,2}$"].concat(exclude).uniq
-    FileUtils.mkdir(destination) unless Dir.exists?(destination)
+    FileUtils.mkdir(destination) unless File.exists?(destination)
     Dir.foreach(source) do |file|
       next if exclude.detect { |pattern| file.match(/#{pattern}/i) }
 
@@ -77,7 +77,7 @@ class Dir
       destination_path = File.join(destination, file)
 
       if File.directory?(source_path)
-        FileUtils.mkdir(destination_path) unless Dir.exists?(destination_path)
+        FileUtils.mkdir(destination_path) unless File.exists?(destination_path)
         clone! source_path, destination_path, exclude
       else
         FileUtils.cp(source_path, destination_path)
